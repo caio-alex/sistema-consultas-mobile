9cd9452e2beff3fdeda40b04360e17970ba68fab
caio-alex <caio62858@gmail.com>
2026-03-17 10:06:48 -0300

    feat: parte 3 componentização

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+
+
+export {default as Consulta} from "./consultaCard";
+
+/**
+ * Conforme criarmos mais componentes, adicionaremos aqui:
+ * 
+ * export { default as Loading } from "./Loading";
+ * export { default as EmptyState } from "./EmptyState";
+ * export { default as Header } from "./Header";
+ */
